31605b52e86b397a112e6961d10f3b950d3439b4
Bethany <bethanychn3@gmail.com>
2023-08-09 17:32:57 -0700

    Submission Completed

--- f.py
+++ f.py
@@ -1,7 +1,8 @@
 # file for any functions created
 import random
+#import o
 
-def key_maker() -> str:
+def make_key() -> str:
     selection = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*_-?<>+"
     new_key = ""
     count = 0
@@ -9,3 +10,35 @@ def key_maker() -> str:
         new_key += random.choice(selection)
         count += 1
     return new_key
+
+def read_file():
+    f = open("SAVED.txt", "r")
+    return f
+
+def verify_key(file, new_key) -> bool:
+    for line in file:
+        l = line.split(" ")
+        #print(l[0])
+        if l[0] == new_key:
+            return False
+    return True
+
+#fi = read_file()
+#print(verify_key(fi, "adf"))
+
+def write_file(key, info):
+    file = open("SAVED.txt", "a")
+    line = key + " " + info + "\n"
+    file.write(line)
+    file.close()
+
+#write_file("abc", "test")
+
+def check_capacity():
+    file = read_file()
+    count = 0
+    for l in file:
+        count += 1
+    if count >= 20:
+        return "full"
+    return "available"

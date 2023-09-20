7fda297868a070cd1a6b15418256651ff673e09e
Bethany Chang <bethansc@uci.edu>
2023-09-19 17:22:44 -0700

    Wrote the rest of the code, was unable to get the hashing to correctly output

--- f.py
+++ f.py
@@ -2,33 +2,42 @@
 import random
 #import o
 
-def make_key() -> str:
-    selection = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*_-?<>+"
-    new_key = ""
-    count = 0
-    while count < 9:
-        new_key += random.choice(selection)
-        count += 1
-    return new_key
+# def make_key() -> str:
+#     selection = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*_-?<>+"
+#     new_key = ""
+#     count = 0
+#     while count < 9:
+#         new_key += random.choice(selection)
+#         count += 1
+#     return new_key
+def make_hash(new_str):
+    new_str2 = new_str.strip()
+    hash_value = hash(new_str2)
+    return hash_value
 
 def read_file():
     f = open("SAVED.txt", "r")
     return f
 
-def verify_key(file, new_key) -> bool:
+def verify_pass(file, new_pass) -> str:
+    hash_val = new_pass.strip()
+    new_hash = hash(hash_val)
+    print(new_hash)
     for line in file:
         l = line.split(" ")
-        #print(l[0])
-        if l[0] == new_key:
-            return False
-    return True
+        print(l[0])
+        if l[0] == new_hash:
+            return l[1]
+    #print(hash("password"))
+    #print(hash("password"))
+    return "NOT FOUND"
 
 #fi = read_file()
 #print(verify_key(fi, "adf"))
 
-def write_file(key, info):
+def write_file(hash_key, info):
     file = open("SAVED.txt", "a")
-    line = key + " " + info + "\n"
+    line = f"{hash_key} {info}\n"
     file.write(line)
     file.close()
 
--- main.py
+++ main.py
@@ -2,15 +2,40 @@
 import f
 #import o
 
-def generate_key():
-    flag = False
-    while flag is False:
-        new_key = f.make_key()
-        file = f.read_file()
-        if f.verify_key(file, new_key):
-            flag = True
-            return new_key
+# def generate_key():
+#     flag = False
+#     while flag is False:
+#         new_key = f.make_key()
+#         file = f.read_file()
+#         if f.verify_key(file, new_key):
+#             flag = True
+#             return new_key
+def store_secret():
+    data = input("Please Enter The information You Would Like to Encrypt: ")
+    password = input("Please Enter an Access Code: ")
+    #key = generate_key()
+    new_hash = f.make_hash(password)
+    f.write_file(new_hash, data)
 
+def find_secret():
+    user_pass = input("Please enter your access code: ")
+    file_open = f.read_file()
+    verification = f.verify_pass(file_open, user_pass)
+    if verification == "NOT FOUND":
+        print("Sorry, the password does not match with any stored information in the database.")
+    else:
+        print(f"Your Secret: {verification}")
+
+def run_processes():
+    print("Here's a List of Available Options: ")
+    print("[1] Store Data \n[2] Find Data\n")
+    picked = input("Please input the corresponding number of your selected option: ")
+    while (picked != "1") and (picked != "2"):
+        picked = input("Please input the corresponding number of your selected option: ")
+    if picked == "1":
+        store_secret()
+    else:
+        find_secret()
 
 def main():
     print("MOCHICRYPT AN UNOFFICIAL ENCRYPTION AND STORAGE :)")
@@ -19,10 +44,7 @@ def main():
         print("Sorry, Mochicrypt is at MAX CAPACITY.")
         return
     else:
-        data = input("Please Enter The information You Would Like to Encrypt: ")
-        key = generate_key()
-        f.write_file(key, data)
-        print(f"YOUR NEW KEY IS {key}")
+        run_processes()
         print("Thank you!")
 
     #print("hello world")
